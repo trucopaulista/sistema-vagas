a8f81a306074071fba77fd8026acfd7d321aeb58
Truco <trucopaulista2301@gmail.com>
2019-04-16 13:35:47 -0300

    Instalando o materilize no projeto

--- src/config/app.use.js
+++ src/config/app.use.js
@@ -1,6 +1,9 @@
-const bodyParser= require('body-parser');
+const bodyParser    = require('body-parser');
+const express       = require('express');
+const path          = require('path');
 
 module.exports = (app) => {
 
     app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(express.static(path.resolve(__dirname, '..', '..', 'public')));
 }
