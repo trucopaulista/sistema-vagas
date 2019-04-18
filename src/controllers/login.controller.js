46ee09c050d29be47a505b8e1f74c6ae1713a487
Truco <trucopaulista2301@gmail.com>
2019-04-18 14:51:04 -0300

    Criando um autenticação simples

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -3,13 +3,32 @@ let api     = {};
 
 api.list = (req, res) => {
 
+    console.log('Tela de login');
     res.render('pages/admin/login/login');
 }
 
 api.login = async (req, res) => {
 
-    console.log('Realizando o login');
     console.log(req.body);
+
+    const { login, password } = req.body;
+    const user =  await model.get(`select * from admins WHERE login = '${ login }' and password = '${ password }' `);
+    
+    try {
+    
+        if(user) {
+            
+            console.log('autenticado');
+            res.redirect('/admin/dashboard');
+            res.json({ auth: user.login });
+        } else {
+            console.log('Login ou senha inválidos');
+            res.json({ fail: 'Login ou senha inválidos' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json(error)
+    }
     
 }
 
